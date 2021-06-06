f0eef5b88386d1b467e4d004c7240178cd5fbeaf
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-06-06 16:16:04 +0100

    Adding a Users List Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import AddUser from './components/Users/AddUser'
+import UsersList from './components/Users/UsersList'
 
 import './mvp.css'
 function App() {
-  return <AddUser />
+  return (
+    <div>
+      <AddUser />
+      <UsersList users={[]} />
+    </div>
+  )
 }
 
 export default App
